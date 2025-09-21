318880effdb8272ae257438f7074e877f57b883f
Jairus <limitlessgreen@outlook.de>
2025-09-21 16:19:07 +0000

    Add missing member onboarding profile fields

---
+++ prisma/migrations/20260109120000_member_onboarding_optional_fields/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "public"."MemberOnboardingProfile"
+    ADD COLUMN "gender" TEXT,
+    ADD COLUMN "memberSinceYear" INTEGER,
+    ADD COLUMN "dietaryPreference" TEXT,
+    ADD COLUMN "dietaryPreferenceStrictness" TEXT;
