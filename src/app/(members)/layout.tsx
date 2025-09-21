ea04bcc062e09326f1661f0383ea3fc1088ce045
Jairus <limitlessgreen@outlook.de>
2025-09-21 08:36:30 +0000

    Make members sidebar scrollable on large screens

--- src/app/(members)/layout.tsx
+++ src/app/(members)/layout.tsx
@@ -14,7 +14,7 @@ export default async function MembersLayout({ children }: { children: React.Reac
       <div
         className="mx-auto flex w-full max-w-screen-2xl flex-col gap-6 px-4 sm:px-6 lg:grid lg:grid-cols-[280px_minmax(0,1fr)] lg:items-start lg:gap-10 lg:px-8 xl:grid-cols-[300px_minmax(0,1fr)] xl:gap-12 xl:px-10 2xl:grid-cols-[320px_minmax(0,1fr)] 2xl:gap-16 2xl:px-12"
       >
-        <aside className="lg:sticky lg:top-28 lg:h-fit lg:self-start">
+        <aside className="lg:sticky lg:top-28 lg:h-fit lg:self-start lg:max-h-[calc(100vh-7rem)] lg:overflow-y-auto lg:pr-2">
           <MembersNav permissions={permissions} activeProduction={activeProduction ?? undefined} />
         </aside>
         <section className="min-w-0 space-y-8">{children}</section>
