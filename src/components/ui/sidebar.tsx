d32d8fc8a3e9bb456aa30240263e98d389a1ff22
Jairus <limitlessgreen@outlook.de>
2025-09-23 18:23:40 +0200

    Fix sidebar width variables

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -178,7 +178,7 @@ const Sidebar = React.forwardRef<
       return (
         <div
           className={cn(
-            "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground",
+            "flex h-full w-[var(--sidebar-width)] flex-col bg-sidebar text-sidebar-foreground",
             className,
           )}
           ref={ref}
@@ -195,7 +195,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[var(--sidebar-width)] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
             style={{ "--sidebar-width": SIDEBAR_WIDTH_MOBILE } as React.CSSProperties}
             side={side}
           >
@@ -220,23 +220,23 @@ const Sidebar = React.forwardRef<
       >
         <div
           className={cn(
-            "relative w-[--sidebar-width] min-h-[calc(100dvh-var(--header-height))] bg-transparent transition-[width] duration-200 ease-linear",
+            "relative w-[var(--sidebar-width)] min-h-[calc(100dvh-var(--header-height))] bg-transparent transition-[width] duration-200 ease-linear",
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
               ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4))]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon]",
+              : "group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)]",
           )}
         />
         <div
           className={cn(
-            "fixed bottom-0 top-[var(--header-height)] z-10 hidden h-[calc(100dvh-var(--header-height))] w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear lg:flex",
+            "fixed bottom-0 top-[var(--header-height)] z-10 hidden h-[calc(100dvh-var(--header-height))] w-[var(--sidebar-width)] transition-[left,right,width] duration-200 ease-linear lg:flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
             variant === "floating" || variant === "inset"
               ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l",
+              : "group-data-[collapsible=icon]:w-[var(--sidebar-width-icon)] group-data-[side=left]:border-r group-data-[side=right]:border-l",
             className,
           )}
           {...props}
