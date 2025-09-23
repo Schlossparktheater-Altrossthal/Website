0efd84d48e9e072f5da9eeee46bfb1c133e8236f
Jairus <limitlessgreen@outlook.de>
2025-09-23 11:46:58 +0200

    Adjust sidebar height below header

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -220,7 +220,7 @@ const Sidebar = React.forwardRef<
       >
         <div
           className={cn(
-            "relative w-[--sidebar-width] bg-transparent transition-[width] duration-200 ease-linear",
+            "relative w-[--sidebar-width] min-h-[calc(100dvh-var(--header-height))] bg-transparent transition-[width] duration-200 ease-linear",
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
@@ -230,7 +230,7 @@ const Sidebar = React.forwardRef<
         />
         <div
           className={cn(
-            "fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear lg:flex",
+            "fixed bottom-0 top-[var(--header-height)] z-10 hidden h-[calc(100dvh-var(--header-height))] w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear lg:flex",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
