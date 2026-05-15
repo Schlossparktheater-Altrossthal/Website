e3c01cb8a759d72600e0a833f73e4f58fdad16f1
Alexander <alexzimmer07@icloud.com>
2026-05-15 15:26:19 +0200

    Stop mounting realtime and frontend editing providers globally

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,9 +4,7 @@ import { Toaster } from "sonner";
 import * as React from "react";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
-import { FrontendEditingProvider } from "@/components/frontend-editing/frontend-editing-provider";
 import { useWebVitals } from "@/hooks/useWebVitals";
-import { RealtimeProvider } from "@/hooks/useRealtime";
 import { OfflineSyncStatusProvider } from "@/lib/offline/hooks";
 import { OfflineSyncProvider as OfflineStorageProvider } from "@/lib/offline/storage";
 import { PwaProvider } from "@/lib/pwa/register-sw";
@@ -33,18 +31,14 @@ export function Providers({
         <OfflineStorageProvider>
           <OfflineSyncStatusProvider authToken={syncToken}>
             <PwaProvider>
-              <RealtimeProvider>
-                <FrontendEditingProvider>
-                  {children}
-                  <Toaster
-                    richColors
-                    position="top-right"
-                    expand={true}
-                    visibleToasts={5}
-                    gap={8}
-                  />
-                </FrontendEditingProvider>
-              </RealtimeProvider>
+              {children}
+              <Toaster
+                richColors
+                position="top-right"
+                expand={true}
+                visibleToasts={5}
+                gap={8}
+              />
             </PwaProvider>
           </OfflineSyncStatusProvider>
         </OfflineStorageProvider>
