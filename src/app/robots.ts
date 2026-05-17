55005d9e1ae71d9410353e6e5e926a4318f86966
Alexander <alexzimmer07@icloud.com>
2026-05-17 22:03:48 +0200

    fix: force dynamic robots route generation

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { readWebsiteSettings, resolveWebsiteSettings } from "@/lib/website-settings";
 
+export const dynamic = "force-dynamic";
+
 const SITEMAP_URL = "https://sommertheater-altrossthal.de/sitemap.xml";
 
 const publicRouteMap = {
