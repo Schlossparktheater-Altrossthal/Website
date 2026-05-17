8efedca98177ae68ee0302827b70756f56f4bd1d
Alexander <alexzimmer07@icloud.com>
2026-05-17 22:11:57 +0200

    fix: force dynamic sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { readWebsiteSettings, resolveWebsiteSettings } from "@/lib/website-settings";
 
+export const dynamic = "force-dynamic";
+
 const BASE_URL = (process.env.NEXT_PUBLIC_BASE_URL ?? "https://sommertheater-altrossthal.de").replace(/\/$/, "");
 
 const publicRouteMap = {
